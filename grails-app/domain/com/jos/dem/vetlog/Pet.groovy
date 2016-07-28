713ca4119c5937ba29469f0e57ecf14d59dab3ea
josdem <joseluis.delacruz@gmail.com>
2016-07-27 21:20:39 -0500

    #1 Adding pet domain

---
+++ grails-app/domain/com/jos/dem/vetlog/Pet.groovy
@@ -0,0 +1,18 @@
+package com.jos.dem.vetlog
+
+class Pet {
+
+  String name
+  Integer age
+  Boolean dewormed = false
+  Boolean sterilized = false
+  Boolean vaccinated = false
+  Breed breed
+
+  static constraints = {
+    name blank:false,nullable:false
+    age min:0,max:300,nullable:false
+  }
+
+}
+
