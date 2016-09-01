de47c3794dba202c49b526040d9fe4e110302588
josdem <joseluis.delacruz@gmail.com>
2016-08-31 21:22:39 -0500

    #6 Adding user creation in the bootstrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,24 @@
 class BootStrap {
 
-    def init = { servletContext ->
+  def init = { servletContext ->
+    if (!User.count()) {
+      createUserWithRole('josdem', '12345678', 'joseluis.delacruz@gmail.com', 'ROLE_USER')
     }
-    def destroy = {
+  }
+
+  def createUserWithRole(String username, String password, String email, String authority) {
+    if(Environment.current != Environment.PRODUCTION){
+      def userRole = Role.findByAuthority(authority)
+      def user = User.findByUsername(username) ?: new User(username:username,
+        password:password,
+        enabled:true,
+        profile:new Profile(name:username, lastName:'lastName', email:email)).save()
+      if(!UserRole.get(user.id,userRole.id)){
+        UserRole.create user, userRole, true
+      }
     }
+  }
+
+  def destroy = {
+  }
 }
