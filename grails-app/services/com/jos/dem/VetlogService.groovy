7d3cba75ea4add7ef2fc6106b29156b40ba58453
josdem <joseluis.delacruz@gmail.com>
2016-09-11 19:21:42 -0500

    #9 Creating flow save user

--- grails-app/services/com/jos/dem/VetlogService.groovy
+++ grails-app/services/com/jos/dem/VetlogService.groovy
@@ -5,7 +5,17 @@ import grails.transaction.Transactional
 @Transactional
 class VetlogService {
 
-    def serviceMethod() {
+  User createUser(UserCommand command){
+    User user = new User(username: command.username, password:command.password)
+    user.profile = command.getProfile()
+    Role role = Role.findWhere(authority: 'ROLE_USER')
+    save(user, userRole)
+    user
+  }
 
-    }
+  void save(User user, Role role){
+    user.save()
+    new UserRole(user:user, role,role).save()
+  }
+
 }
