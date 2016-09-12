6028ba514ce170a90091a18e88f575ff25f788f0
josdem <joseluis.delacruz@gmail.com>
2016-09-11 19:06:07 -0500

    #9 Generating flow to render errors or save user

--- grails-app/controllers/com/jos/dem/vetlog/UserController.groovy
+++ grails-app/controllers/com/jos/dem/vetlog/UserController.groovy
@@ -6,102 +6,94 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class UserController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+  static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond User.list(params), model:[userCount: User.count()]
+  def index(Integer max) {
+    params.max = Math.min(max ?: 10, 100)
+    respond User.list(params), model:[userCount: User.count()]
+  }
+
+  def show(User user) {
+    respond user
+  }
+
+  def create() {
+    respond new User(params)
+  }
+
+  @Transactional
+  def save(UserCommand command) {
+    if (command.hasErrors()) {
+      render(view:'create', model:[user:command])
     }
 
-    def show(User user) {
-        respond user
+    User user = userService.saveUser(command)
+
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), user.id])
+        redirect user
+      }
+      '*' { respond user, [status: CREATED] }
+    }
+  }
+
+  def edit(User user) {
+    respond user
+  }
+
+  @Transactional
+  def update(User user) {
+    if (user == null) {
+      transactionStatus.setRollbackOnly()
+      notFound()
+      return
     }
 
-    def create() {
-        respond new User(params)
+    if (user.hasErrors()) {
+      transactionStatus.setRollbackOnly()
+      respond user.errors, view:'edit'
+      return
     }
 
-    @Transactional
-    def save(User user) {
-        if (user == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
+    user.save flush:true
 
-        if (user.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond user.errors, view:'create'
-            return
-        }
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), user.id])
+        redirect user
+      }
+      '*'{ respond user, [status: OK] }
+    }
+  }
 
-        user.save flush:true
+  @Transactional
+  def delete(User user) {
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), user.id])
-                redirect user
-            }
-            '*' { respond user, [status: CREATED] }
-        }
+    if (user == null) {
+      transactionStatus.setRollbackOnly()
+      notFound()
+      return
     }
 
-    def edit(User user) {
-        respond user
+    user.delete flush:true
+
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), user.id])
+        redirect action:"index", method:"GET"
+      }
+      '*'{ render status: NO_CONTENT }
     }
+  }
 
-    @Transactional
-    def update(User user) {
-        if (user == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        if (user.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond user.errors, view:'edit'
-            return
-        }
-
-        user.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), user.id])
-                redirect user
-            }
-            '*'{ respond user, [status: OK] }
-        }
+  protected void notFound() {
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
+        redirect action: "index", method: "GET"
+      }
+      '*'{ render status: NOT_FOUND }
     }
-
-    @Transactional
-    def delete(User user) {
-
-        if (user == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        user.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), user.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+  }
 }
