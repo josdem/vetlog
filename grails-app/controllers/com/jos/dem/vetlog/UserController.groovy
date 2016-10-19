4ab2ada2620a773a6f9b424ac96cc108a0e3727f
josdem <joseluis.delacruz@gmail.com>
2016-10-18 20:15:55 -0500

    Adding call to the service

--- grails-app/controllers/com/jos/dem/vetlog/UserController.groovy
+++ grails-app/controllers/com/jos/dem/vetlog/UserController.groovy
@@ -8,6 +8,8 @@ class UserController {
 
   static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
+  UserService userService
+
   def index(Integer max) {
     params.max = Math.min(max ?: 10, 100)
     respond User.list(params), model:[userCount: User.count()]
@@ -27,7 +29,7 @@ class UserController {
       render(view:'create', model:[user:command])
     }
 
-    User user = userService.saveUser(command)
+    User user = userService.createUser(command)
 
     request.withFormat {
       form multipartForm {
