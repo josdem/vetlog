458867e0124b050a733158bc7f8f4bb0d5448c18
josdem <joseluis.delacruz@gmail.com>
2016-10-23 21:13:39 -0500

    #13 Adding externalization properties functionality

--- grails-app/init/vetlog/Application.groovy
+++ grails-app/init/vetlog/Application.groovy
@@ -2,9 +2,28 @@ package vetlog
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.EnvironmentAware
+import org.springframework.core.env.Environment
+import org.springframework.core.env.MapPropertySource
+import org.springframework.core.env.PropertiesPropertySource
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.Resource
+import org.springframework.context.annotation.ComponentScan
 
-class Application extends GrailsAutoConfiguration {
-    static void main(String[] args) {
-        GrailsApp.run(Application, args)
+@ComponentScan("com.jos.dem.vetlog.config")
+class Application extends GrailsAutoConfiguration implements EnvironmentAware {
+  static void main(String[] args) {
+    GrailsApp.run(Application, args)
+  }
+
+  void setEnvironment(Environment environment) {
+    def configBase = new File("${System.getProperty('user.home')}/.vetlog/application-${environment.activeProfiles[0]}.groovy")
+    if(configBase.exists()) {
+      println "Loading external configuration from Groovy: ${configBase.absolutePath}"
+      def config = new ConfigSlurper().parse(configBase.toURL())
+      environment.propertySources.addFirst(new MapPropertySource("externalGroovyConfig", config))
+    } else {
+      println "External config could not be found ${configBase.absolutePath}"
     }
+  }
 }
--- grails-app/services/com/jos/dem/vetlog/RecoveryCollaboratorService.groovy
+++ grails-app/services/com/jos/dem/vetlog/RecoveryCollaboratorService.groovy
@@ -5,11 +5,10 @@ import grails.transaction.Transactional
 @Transactional
 class RecoveryCollaboratorService {
 
-  def generateToken(String baseUrl, String email){
-    def registration = new RegistrationCode(email:email)
+  MessageCommand generateToken(String baseUrl, String email){
+    RegistrationCode registration = new RegistrationCode(email:email)
     registration.save()
-    def message = new TokenCommand(email:email, token:"${baseUrl}${registration.token}")
-    message
+    new TokenCommand(email:email, token:"${baseUrl}${registration.token}")
   }
 
 }
