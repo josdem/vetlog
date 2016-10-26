aed9d19d973f0d38fa63823b0bf9b2797684bf68
josdem <joseluis.delacruz@gmail.com>
2016-10-25 21:39:35 -0500

    #14 Adding rest exception

---
+++ grails-app/exceptions/RestException.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.vetlog
+
+class RestException extends RuntimeException {
+
+  RestException(String msg){
+    super(msg)
+  }
+
+  String getMessage(){
+    super.message
+  }
+
+}
