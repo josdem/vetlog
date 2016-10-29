b2706247f6706534b749ee38f896dcf1a6b08501
josdem <joseluis.delacruz@gmail.com>
2016-10-29 10:15:37 -0500

    #14 Adding message command parameter

--- grails-app/services/com/jos/dem/vetlog/RecoveryService.groovy
+++ grails-app/services/com/jos/dem/vetlog/RecoveryService.groovy
@@ -4,11 +4,12 @@ import grails.transaction.Transactional
 
 class RecoveryService {
 
+  def restService
   def grailsApplication
   def recoveryCollaboratorService
 
   def sendConfirmationAccountToken(String email) {
-    recoveryCollaboratorService.generateToken("${grailsApplication.config.recovery.register}", email)
+    MessageCommand message = recoveryCollaboratorService.generateToken("${grailsApplication.config.recovery.register}", email)
     restService.sendCommand(message, grailsApplication.config.emailer.register)
   }
 
