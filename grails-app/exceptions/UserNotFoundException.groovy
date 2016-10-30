45678a11b5d3f6fe51afd8c1f832a60c8356f84e
josdem <joseluis.delacruz@gmail.com>
2016-10-30 17:54:04 -0600

    #15 Adding user not found exception

---
+++ grails-app/exceptions/UserNotFoundException.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.vetlog
+
+class UserNotFoundException extends RuntimeException {
+
+  UserNotFoundException(String msg){
+    super(msg)
+  }
+
+  String getMessage(){
+    super.message
+  }
+
+}
