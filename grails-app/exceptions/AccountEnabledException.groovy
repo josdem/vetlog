e3b6bc42fc36e4d93c2fd978dd1be3a46b0d3e14
josdem <joseluis.delacruz@gmail.com>
2016-10-30 17:55:10 -0600

    #15 Adding account enabled exception

---
+++ grails-app/exceptions/AccountEnabledException.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.vetlog
+
+class AccountEnabledException extends RuntimeException {
+
+  AccountEnabledException(String msg){
+    super(msg)
+  }
+
+  String getMessage(){
+    super.message
+  }
+
+}
